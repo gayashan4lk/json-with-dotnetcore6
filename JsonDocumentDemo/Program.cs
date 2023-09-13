94188d22a69406edd5157f4e41cb9503f4df986e
gayashan4lk <gayashan4lk@gmail.com>
2023-09-14 04:24:32 +0530

    how to use JsonDocument

---
+++ JsonDocumentDemo/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Text.Json;
+
+namespace JsonDocumentDemo
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			var fileName = "weather.json";
+			var filePath = $"..\\..\\..\\{fileName}";
+
+			string jsonStr = File.ReadAllText(filePath);
+
+			using (JsonDocument doc = JsonDocument.Parse(jsonStr))
+			{
+				JsonElement root = doc.RootElement;
+
+				//Console.WriteLine(root.ToString());
+
+				JsonElement temp = root.GetProperty("temperaturecelsius");
+				//Console.WriteLine(temp.ToString());
+
+				JsonElement last7days = root.GetProperty("last7days");
+				//Console.WriteLine(last7days.ToString());
+
+				foreach (var measure in last7days.EnumerateArray())
+				{
+					if (measure.TryGetProperty("temperature", out JsonElement dayTemp))
+					{
+						Console.WriteLine($"Temp: {dayTemp.ToString()}");
+					}
+
+					if (measure.TryGetProperty("humidity", out JsonElement dayHumidity))
+					{
+						Console.Write($"Humidity: {dayHumidity.ToString()}\n");
+					}
+				}
+
+			}
+		}
+	}
+}
---
+++ JsonDocumentDemo/TempDataPoint.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JsonDocumentDemo
+{
+	public class TempDataPoint
+	{
+		public int temperature { get; set; }
+		public int humidity { get; set; }
+	}
+}
